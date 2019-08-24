ceeeca8492d524cbeaa1dd192cd2b9c9a4891303
JoelWantsCoffee <joelwrichardson01@gmail.com>
2019-08-24 11:42:45 +1000

    buttons

--- src/cool-files/Start.java
+++ src/cool-files/Start.java
@@ -1,6 +1,11 @@
 import processing.core.*;
 
 public class Start extends PApplet {
+    boolean cencored = false;
+    boolean mouseDown = false;
+    boolean mousePressed = false;
+    boolean mouseReleased = false;
+
     public static void main(String[] args) {
         String[] appletArgs = new String[] { "Start" };
         PApplet.main(appletArgs);
@@ -11,14 +16,28 @@ public class Start extends PApplet {
     }
 
     public void draw() {
-        Screen srn = new Screen();
+
+	Screen srn = new Screen();
         srn.drawScreen();
         fill(random(255), random(255), random(255));
         ellipse(mouseX, mouseY, 50, 50);
+
+	mousePressed = false;
+	mouseReleased = false;
     }
 
     public void keyReleased() {
         exit();
+    }
+
+    public void mousePressed() {
+	mousePressed = true;
+	mouseDown = true;
+    }
+
+    public void mouseReleased() {
+	mouseReleased = true;
+	mouseDown = false;
     }
 
     public class Screen {
@@ -35,5 +54,28 @@ public class Start extends PApplet {
             noStroke();
             rect(s, s, width - s * 2, height - s * 2);
         }
+    }
+
+    public class Button {
+	public int x, y, width, height;
+	public Button(int x, int y, int width, int height) {
+	    this.x = x;
+	    this.y = y;
+	    this.width = width;
+	    this.height = height;
+	}
+
+	public boolean mouseOver() {
+	    if ((mouseX < x + width) && (mouseY < y + height)) {
+		if (mouseX > x && mouseY < y) {
+		    return true;
+		}
+	    }
+	    return false;
+	}
+
+	public boolean clicked() {
+	    return (mouseOver() && mouseReleased);
+	}
     }
 }
